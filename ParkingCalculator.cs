a860727055685da1f2ca398de5276215fd9cd905
Jarek <jaroslaw.pela@wp.pl>
2023-11-06 22:12:42 +0100

    Add project files.

---
+++ ParkingCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingCalculator
+{
+    public class ParkingCalculator
+    {
+        public static double CalculateParkingFee(int hours)
+        {
+            double result = 0;
+            // TODO: Uzupełnij implementację kalkulatora opłat za parkowanie
+
+            return result;
+        }
+    }
+}
