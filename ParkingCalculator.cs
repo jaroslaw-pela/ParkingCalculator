ff7f08d7dd04e6f3f6b5c8019fa5823bdba679bb
Jarek <jaroslaw.pela@wp.pl>
2023-11-06 22:30:55 +0100

    first test application

--- ParkingCalculator.cs
+++ ParkingCalculator.cs
@@ -13,6 +13,20 @@ namespace ParkingCalculator
             double result = 0;
             // TODO: Uzupełnij implementację kalkulatora opłat za parkowanie
 
+            switch (hours)
+            {
+
+                case (<= 1):
+                    result = 5;
+                    break;
+                case (> 1):
+                    result = (hours * 3) + 2;
+                    break;
+                default:
+                    Console.WriteLine("Wrong value");
+                    break;
+
+            }
 
             return result;
         }
