fc502f5f8ebc182911962c3845cdc1ea5b0c311f
Jarek <jaroslaw.pela@wp.pl>
2023-11-08 00:17:57 +0100

    added hours validation

--- Program.cs
+++ Program.cs
@@ -6,17 +6,29 @@
         {
             Console.WriteLine("   ___           _    _                        \r\n  / _ \\__ _ _ __| | _(_)_ __   __ _            \r\n / /_)/ _` | '__| |/ | | '_ \\ / _` |           \r\n/ ___| (_| | |  |   <| | | | | (_| |           \r\n\\/    \\__,_|_|  |_|\\_|_|_| |_|\\__, |           \r\n                              |___/            \r\n   ___      _            _       _             \r\n  / __\\__ _| | ___ _   _| | __ _| |_ ___  _ __ \r\n / /  / _` | |/ __| | | | |/ _` | __/ _ \\| '__|\r\n/ /__| (_| | | (__| |_| | | (_| | || (_) | |   \r\n\\____/\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|   \r\n                                               ");
 
+            int hours = 0;
 
-            Console.WriteLine("Enter how many hours you want to park\n");
+            while (true)
+            {
+                Console.WriteLine("Enter how many hours you want to park\n");
+                try
+                {
+                    var enter = Console.ReadLine();
+                    hours = int.Parse(enter);
 
-            int hours = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("   ___           _    _                        \r\n  / _ \\__ _ _ __| | _(_)_ __   __ _            \r\n / /_)/ _` | '__| |/ | | '_ \\ / _` |           \r\n/ ___| (_| | |  |   <| | | | | (_| |           \r\n\\/    \\__,_|_|  |_|\\_|_|_| |_|\\__, |           \r\n                              |___/            \r\n   ___      _            _       _             \r\n  / __\\__ _| | ___ _   _| | __ _| |_ ___  _ __ \r\n / /  / _` | |/ __| | | | |/ _` | __/ _ \\| '__|\r\n/ /__| (_| | | (__| |_| | | (_| | || (_) | |   \r\n\\____/\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|   \r\n                                               ");
 
+                    Console.WriteLine($"The parking fee is: {ParkingCalculator.CalculateParkingFee(hours)} zł.\n\n");
 
-            Console.Clear();
-
-            Console.WriteLine("   ___           _    _                        \r\n  / _ \\__ _ _ __| | _(_)_ __   __ _            \r\n / /_)/ _` | '__| |/ | | '_ \\ / _` |           \r\n/ ___| (_| | |  |   <| | | | | (_| |           \r\n\\/    \\__,_|_|  |_|\\_|_|_| |_|\\__, |           \r\n                              |___/            \r\n   ___      _            _       _             \r\n  / __\\__ _| | ___ _   _| | __ _| |_ ___  _ __ \r\n / /  / _` | |/ __| | | | |/ _` | __/ _ \\| '__|\r\n/ /__| (_| | | (__| |_| | | (_| | || (_) | |   \r\n\\____/\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|   \r\n                                               ");
-
-            Console.WriteLine($"The parking fee is: {ParkingCalculator.CalculateParkingFee(hours)} zł.");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + " Please try again.\n\n");
+                    continue;
+                }
+            }
         }
     }
 }
